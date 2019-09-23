5e310a97eedc9f65368490a78bbf2ea729307952
Senior-Pomidor <bobylev.vladi@gmail.com>
2019-09-23 14:16:05 +0300

    add Esc handler

--- form.js
+++ form.js
@@ -1,8 +1,9 @@
 let formSection = document.querySelector('.write-us');
-let formWrapper = document.querySelector('.wright-us__wrapper');
-let form = document.querySelector('.write-us__form');
+let formWrapper = formSection.querySelector('.wright-us__wrapper');
+let form = formWrapper.querySelector('.write-us__form');
 let submitButton = form.querySelector('.write-us__button');
 let successMessage = formWrapper.querySelector('.success-message');
+let formInputs = form.querySelectorAll('.write-us__input');
 // submitButton.addEventListener('click', function() {
 //    alert('Сообщение отправлено!');
 // });
@@ -14,7 +15,28 @@ form.addEventListener('submit', function() {
    formSection.style.display = 'none';
    successMessage.classList.add('hidden');
 },2000)
+   // for(let i = 0; i < formInputs.length; i++){
+   //    formInputs[i].value = '';
+   // }
+   // form.querySelector('.write-us__textarea').value = '';
+   emptyInputs();
 });
+
+function emptyInputs() {
+   for(let i = 0; i < formInputs.length; i++){
+      formInputs[i].value = '';
+   }
+   form.querySelector('.write-us__textarea').value = '';
+};
+
+document.addEventListener('keydown', function(evt) {
+   if(evt.keyCode === 27) {
+      formSection.style.display = 'none';
+      emptyInputs();
+   }
+});
+
+
 
 // function formClose() {
 //    formSection.style.display = 'none';
